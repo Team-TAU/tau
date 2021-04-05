14fe9a7caa7f80140cdc2c89799e17337bbb12cb
FiniteSingularity <finitesingularityttv@gmail.com>
2021-04-05 22:28:26 +0000

    Fixed twitch callback url for non localhost

--- tau/core/views.py
+++ tau/core/views.py
@@ -4,6 +4,7 @@ import requests
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.template import loader
 from django.contrib.auth import login
+from django.conf import settings
 
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
@@ -91,12 +92,6 @@ def get_twitch_user(request):
 
 def get_channel_name_view(request):
     if request.method == 'POST':
-        port = os.environ.get('PORT', 8000)
-        if int(port) == 80:
-            port_text = ''
-        else:
-            port_text = f':{port}'
-        base_url = os.environ.get('BASE_URL', 'http://localhost')
         form = ChannelNameForm(request.POST)
         if form.is_valid():
             # Process the data
@@ -104,7 +99,7 @@ def get_channel_name_view(request):
             client_id = os.environ.get('TWITCH_APP_ID', None)
             url = f'https://id.twitch.tv/oauth2/authorize?' \
                   f'client_id={client_id}&' \
-                  f'redirect_uri={base_url}{port_text}/twitch-callback/&' \
+                  f'redirect_uri={settings.BASE_URL}/twitch-callback/&' \
                   f'response_type=code&' \
                   f'scope=bits:read channel:read:redemptions channel:' \
                         f'read:hype_train channel_subscriptions'
@@ -125,12 +120,6 @@ def get_tau_token(request):
         return JsonResponse({'token': token.key})
 
 def process_twitch_callback_view(request):
-    port = int(os.environ.get('PORT', 8000))
-    if int(port) == 80:
-        port_text = ''
-    else:
-        port_text = f':{port}'
-    base_url = os.environ.get('BASE_URL', 'http://localhost')
     params = request.GET
     auth_code = params['code']
     client_id = os.environ.get('TWITCH_APP_ID', None)
@@ -140,7 +129,7 @@ def process_twitch_callback_view(request):
         'client_secret': client_secret,
         'code': auth_code,
         'grant_type': 'authorization_code',
-        'redirect_uri': f'{base_url}{port_text}/twitch-callback/'
+        'redirect_uri': f'{settings.BASE_URL}/twitch-callback/'
     })
     response_data = auth_r.json()
     config.TWITCH_ACCESS_TOKEN = response_data['access_token']
