16afd599fc8fe3b8fb664b2d91fd07010ab99aab
Matthew Foley <mtfoley.mae@gmail.com>
2021-05-12 06:29:30 -0400

    use vue for channel points

--- tau/templates/static/js/app-vue.js
+++ tau/templates/static/js/app-vue.js
@@ -1,8 +1,17 @@
-window.onload = function(){
-    var vueApp = new Vue({
-    el: '#vue-app',
-    data: {
-      message: 'Hello Vue!'
-    }
-  });
-}
+var rewardSelect = new Vue({
+  el:'#reward',
+  delimiters:["[[","]]"],
+  data:{
+    alwaysOptions:[
+      {id:"575c2991-cbc2-402c-837f-86bd40009379", label:"Test"}
+    ],
+    fetchedOptions:[]
+  }
+});
+var rewardErrorMsg = new Vue({
+  el:'#reward-error',
+  delimiters:["[[", "]]"],
+  data:{
+    error:""
+  }
+});
