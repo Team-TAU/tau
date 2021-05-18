39090ea740359b0efffbe0316fd729305495a5f6
FiniteSingularity <finitesingularityttv@gmail.com>
2021-05-18 14:50:02 -0500

    Updated production config file, and requirements.txt to use WhiteNoise for static file in production mode.

--- tau/config/production.py
+++ tau/config/production.py
@@ -5,6 +5,12 @@ from .common import Common
 class Production(Common):
     INSTALLED_APPS = Common.INSTALLED_APPS
 
+    ins = list(Common.MIDDLEWARE).index('django.middleware.security.SecurityMiddleware')
+    if ins:
+        MIDDLEWARE=list(Common.MIDDLEWARE)
+        MIDDLEWARE.insert(ins+1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+        MIDDLEWARE=tuple(MIDDLEWARE)
+    
     # Site
     # https://docs.djangoproject.com/en/2.0/ref/settings/#allowed-hosts
     ALLOWED_HOSTS = ["*"]
