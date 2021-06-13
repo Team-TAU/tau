44b7c2b6efb8be37aa0968eeb06746930594c254
Matthew Foley <mtfoley.mae@gmail.com>
2021-06-13 10:13:23 -0400

    update for readability and smart defaults

--- tau/config/validate.py
+++ tau/config/validate.py
@@ -4,46 +4,40 @@ import os
 import sys
 import logging
 
-# TWITCH SECRET LENGTH 10->100
-# if DJANGO_DB_TYPE=POSTGRES, NEED DJANGO_DB, DJANGO_DB_USER, DJANGO_DB_PW, DJANGO_DB_URL
-# if DJANGO_DB_TYPE=sqlite, nothing else required
-# if USE_NGROK=TRUE, MUST HAVE USE_NGROK_TOKEN. IF USE_NGROK_TOKEN=TRUE, NEED NGROK_TOKEN
-# PROPOSED: USE_NGROK=TRUE BY DEFAULT. IF NGROK_TOKEN IS SET, USE IT.
+def check_exists(errors,name,label):
+    if "" == os.environ.get(name):
+        errors.append(f"Missing environment setting {name} ({label})")
+
+def check_length(errors,name,label,min=10,max=100):
+    value = os.environ.get(name)
+    if(len(value) < min or len(value) > max):
+        errors.append(f"Environment setting {name} must be between {min} and {max} characters.")
 
 def main():
     errors = []
     warnings = []
+    # Twitch Settings
+    check_exists(errors,"TWITCH_APP_ID","Twitch App ID")
+    check_exists(errors,"TWITCH_CLIENT_SECRET","Twitch Client Secret")
+    check_exists(errors,"TWITCH_WEBHOOK_SECRET","Twitch Webhook Secret")
+    
+    # Database Settings
     DB_TYPE = os.environ.get("DJANGO_DB_TYPE","postgres")
     if DB_TYPE == "postgres":
-        if "" == os.environ.get("DJANGO_DB",""):
-            errors.append("Missing environment setting DJANGO_DB (Django Database Name)")
-        if "" == os.environ.get("DJANGO_DB_USER",""):
-            errors.append("Missing environment setting DJANGO_DB_USER (Django Database Username)")
-        if "" == os.environ.get("DJANGO_DB_PW",""):
-            errors.append("Missing environment setting DJANGO_DB_PW (Django Database Password)")
-        if "" == os.environ.get("DJANGO_DB_URL",""):
-            errors.append("Missing environment setting DJANGO_DB_URL (Django Database URL)")
+        check_exists(errors,"POSTGRES_PW","Postgres Root Password")
+        check_exists(errors,"DJANGO_DB_PW","Django Database Password")
+        check_exists(errors,"DJANGO_SECRET_KEY","Django Encryption Key")
     if DB_TYPE != "postgres" and DB_TYPE != "sqlite3":
         errors.append("Invalid environment setting DJANGO_DB_TYPE (Django Database Type). "
         +"Must be equal to \"postgres\" or \"sqlite3\" and is currently set to \""+DB_TYPE+"\"")
 
-    if "" == os.environ.get("TWITCH_APP_ID",""):
-        errors.append("Missing environment setting TWITCH_APP_ID (Twitch App ID)")
-    if "" == os.environ.get("TWITCH_CLIENT_SECRET",""):
-        errors.append("Missing environment setting TWITCH_CLIENT_SECRET (Twitch Client Secret)")
-    TWITCH_WEBHOOK_SECRET = os.environ.get("TWITCH_WEBHOOK_SECRET","")
-    if "" == TWITCH_WEBHOOK_SECRET:
-        errors.append("Missing environment setting TWITCH_WEBHOOK SECRET (Twitch Webhook Secret)")
-    if len(TWITCH_WEBHOOK_SECRET) < 10 or len(TWITCH_WEBHOOK_SECRET) > 100:
-        errors.append("Environment setting TWITCH_WEBHOOK_SECRET must be between 10 and 100 characters")
-
-    if "True" == os.environ.get("USE_NGROK","True") and "" == os.environ.get("NGROK_TOKEN",""):
-        warnings.append("For best results, consider entering an NGROK_TOKEN. This is available with a free sign-up on https://ngrok.io")
-
+    # REDIS Settings
     if "" != os.environ.get("REDIS_ENDPOINT") and "" == os.environ.get("REDIS_PW"):
         errors.append("When environment setting REDIS_ENDPOINT is in use, the setting REDIS_PW must also be set.")
     if "" != os.environ.get("REDIS_SERVER") and "" != os.environ.get("REDIS_ENDPOINT"):
         warnings.append("When environment setting REDIS_ENDPOINT is in use, it will supercede environment setting REDIS_SERVER.")
+
+    # Log warnings and errors. Exit with errors if necessary.
     if len(warnings) > 0:
         [logging.warning(warning) for warning in warnings]
     if len(errors) > 0:
