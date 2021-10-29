fffed8cfac50694b96f73d21d1fd3bab01efae80
FiniteSingularity <finitesingularityttv@gmail.com>
2021-10-29 20:39:45 +0000

    Fixes reconnection issue with ngrok tunnels.

--- tau/core/utils.py
+++ tau/core/utils.py
@@ -72,9 +72,8 @@ def get_all_statuses():
 
 def setup_ngrok():
     # pyngrok will only be installed if it is used.
-    print('importing ngrok')
     from pyngrok import ngrok
-    print('done importing')
+    ngrok.kill()
     print('---- Setting up ngrok tunnel ----')
     # Get the dev server port (defaults to 8000 for Django, can be overridden with the
     # last arg when calling `runserver`)
