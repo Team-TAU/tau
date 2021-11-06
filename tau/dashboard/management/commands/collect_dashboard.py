f549b6d87b69c086784b4fdb257b35ebaf158425
FiniteSingularity <finitesingularityttv@gmail.com>
2021-11-06 12:54:10 +0000

    Adds --build option to collect_dashboard management command.  --build will build the Vue project before copying files over.  This is useful for development.

--- tau/dashboard/management/commands/collect_dashboard.py
+++ tau/dashboard/management/commands/collect_dashboard.py
@@ -6,10 +6,23 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = 'Copies vue generated dashboard html/js/css files to proper django dashboard directories.'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--build',
+            action='store_true',
+            help='Build the dashboard Vue project.  This is only needed during development.'
+        )
+
     def handle(self, *args, **kwargs):
         templates_dir = f'{settings.BASE_DIR}/dashboard/templates/dashboard'
         static_dir = f'{settings.BASE_DIR}/dashboard/static'
-        source_dir = f'{settings.BASE_DIR}/../tau-dashboard/dist'
+        dashboard_project_dir = f'{settings.BASE_DIR}/../tau-dashboard'
+        source_dir = f'{dashboard_project_dir}/dist'
+        print(dashboard_project_dir)
+        if kwargs['build']:
+            print('Building the dashboard Vue project...')
+            os.system(f'bash -c "pushd {dashboard_project_dir}; npm install; npm run build --prod; pushd"')
+
         print('Copying compiled dashboard Vue app to templates/static...')
         os.system(f'mkdir -p {templates_dir}')
         os.system(f'cp -v {source_dir}/index.html {templates_dir}')
