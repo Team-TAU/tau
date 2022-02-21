f8a939ff450f64b27264859b25264eac67a0c939
Finite Singularity <finitesingularityttv@gmail.com>
2022-02-21 16:23:20 +0000

    Finishes removing all nose deps, and converting over to use pytest.

--- tau/config/local.py
+++ tau/config/local.py
@@ -6,14 +6,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 class Local(Common):
     DEBUG = True
     # Testing
-    INSTALLED_APPS = Common.INSTALLED_APPS
-    INSTALLED_APPS += ('django_nose',)
-    TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-    NOSE_ARGS = [
-        BASE_DIR,
-        '-s',
-        '--nologcapture',
-        '--with-coverage',
-        '--with-progressive',
-        '--cover-package=tau'
-    ]
+    # INSTALLED_APPS = Common.INSTALLED_APPS
+    # INSTALLED_APPS += ('django_nose',)
+    # TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+    # NOSE_ARGS = [
+    #     BASE_DIR,
+    #     '-s',
+    #     '--nologcapture',
+    #     '--with-coverage',
+    #     '--with-progressive',
+    #     '--cover-package=tau'
+    # ]
