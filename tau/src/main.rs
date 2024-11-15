9cc3f6e47d80dcbbe6eca63b657e1a9ee085bc66
Sarah Schulte <git@sarah.engineer>
2024-11-15 10:30:47 -0800

    get sqlx working

--- tau/src/main.rs
+++ tau/src/main.rs
@@ -1,3 +1,4 @@
+use axum::http::StatusCode;
 use sqlx::postgres::PgPoolOptions;
 use sqlx::{Pool, Postgres};
 use std::net::Ipv4Addr;
@@ -11,6 +12,23 @@ use utoipa_swagger_ui::SwaggerUi;
 const CUSTOMER_TAG: &str = "customer";
 const AUTH_TAG: &str = "auth";
 const ORDER_TAG: &str = "order";
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum AppError {
+    #[error("uh oh")]
+    Anyhow(#[from] anyhow::Error),
+}
+
+impl axum::response::IntoResponse for AppError {
+    fn into_response(self) -> axum::response::Response {
+        let status = match self {
+            AppError::Anyhow(..) => StatusCode::INTERNAL_SERVER_ERROR,
+        };
+        (status, self.to_string()).into_response()
+    }
+}
 
 #[derive(OpenApi)]
 #[openapi(
@@ -49,15 +67,15 @@ async fn main() -> Result<(), anyhow::Error> {
     let state = RouterState { pool };
 
     let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
-        .with_state(state)
         .routes(routes!(health))
         .nest("/api/v1/auth", auth::router())
         .nest("/api/v1/customer", customer::router())
-        .nest("/api/v1/order", order::router())
+        // .nest("/api/v1/order", order::router())
         .routes(routes!(
             inner::secret_handlers::get_secret,
             inner::secret_handlers::post_secret
         ))
+        .with_state(state)
         .split_for_parts();
 
     let router = router.merge(SwaggerUi::new("/swagger-ui").url("/apidoc/openapi.json", api));
@@ -68,10 +86,11 @@ async fn main() -> Result<(), anyhow::Error> {
 }
 
 mod auth {
+    use anyhow::Context as _;
+    use axum::extract::{Json, State};
     use axum::http::StatusCode;
     use axum::response::IntoResponse;
-    use axum::Json;
-    use serde::Serialize;
+    use serde::{Deserialize, Serialize};
     use utoipa::ToSchema;
     use utoipa_axum::router::OpenApiRouter;
     use utoipa_axum::routes;
@@ -81,28 +100,48 @@ mod auth {
         token: String,
     }
 
-    pub fn router() -> OpenApiRouter {
+    #[derive(ToSchema, Serialize, Deserialize)]
+    struct TokenRequest {
+        username: String,
+        password: String,
+    }
+
+    pub fn router() -> OpenApiRouter<crate::RouterState> {
         OpenApiRouter::new().routes(routes!(post_token_auth))
     }
 
-    #[utoipa::path(post, path = "/token", responses(
+    #[utoipa::path(post, path = "/token",
+        responses(
         (status = OK, body = TokenResponse),
-        (status = FORBIDDEN, body = ())
+        (status = FORBIDDEN, body = ()),
+        (status = 500, body = ())
     ), tag = super::AUTH_TAG)]
-    async fn post_token_auth() -> impl IntoResponse {
-        if
-        (
-            StatusCode::OK,
-            Json(TokenResponse {
-                token: String::from("thisisyourtoken"),
-            }),
+    async fn post_token_auth(
+        State(state): State<crate::RouterState>,
+        Json(payload): Json<TokenRequest>,
+    ) -> Result<impl IntoResponse, crate::AppError> {
+        let row = sqlx::query!(
+            "SELECT id FROM users_user WHERE username = $1",
+            payload.username
         )
-        (
-            StatusCode::OK,
-            Json(TokenResponse {
-                token: String::from("thisisyourtoken"),
-            }),
-        )
+        .fetch_optional(&state.pool)
+        .await
+        .context("")?;
+        if let Some(_) = row {
+            if payload.password == "hunter2" {
+                Ok((
+                    StatusCode::OK,
+                    Json(TokenResponse {
+                        token: String::from("*******"),
+                    }),
+                )
+                    .into_response())
+            } else {
+                Ok(StatusCode::FORBIDDEN.into_response())
+            }
+        } else {
+            Ok(StatusCode::FORBIDDEN.into_response())
+        }
     }
 }
 mod customer {
@@ -119,7 +158,7 @@ mod customer {
     }
 
     /// expose the Customer OpenAPI to parent module
-    pub fn router() -> OpenApiRouter {
+    pub fn router() -> OpenApiRouter<crate::RouterState> {
         OpenApiRouter::new().routes(routes!(get_customer))
     }
 
@@ -154,7 +193,7 @@ mod order {
     }
 
     /// expose the Order OpenAPI to parent module
-    pub fn router() -> OpenApiRouter {
+    pub fn router() -> OpenApiRouter<crate::RouterState> {
         OpenApiRouter::new().routes(routes!(get_order, create_order))
     }
 
